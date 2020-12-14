ee6a0e98e8192ad4c13de0d1fdfbdacf4c5dd3aa
Juhanir <juhani.riisio@gmail.com>
2020-12-14 20:05:10 +0200

    day12

---
+++ src/main/day12/Ship.java
@@ -0,0 +1,117 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ship {
+
+  public enum Direction {
+    EAST, WEST, NORTH, SOUTH
+  }
+
+  private Direction direction;
+  private Map<Direction, Direction> leftTurns;
+  private Map<Direction, Direction> rightTurns;
+  private int x;
+  private int y;
+
+  public Ship(Direction d, int x, int y) {
+    this.direction = d;
+    this.x = x;
+    this.y = y;
+    this.leftTurns = new HashMap<>();
+    this.leftTurns.put(Direction.NORTH, Direction.WEST);
+    this.leftTurns.put(Direction.WEST, Direction.SOUTH);
+    this.leftTurns.put(Direction.SOUTH, Direction.EAST);
+    this.leftTurns.put(Direction.EAST, Direction.NORTH);
+    this.rightTurns = new HashMap<>();
+    this.rightTurns.put(Direction.NORTH, Direction.EAST);
+    this.rightTurns.put(Direction.EAST, Direction.SOUTH);
+    this.rightTurns.put(Direction.SOUTH, Direction.WEST);
+    this.rightTurns.put(Direction.WEST, Direction.NORTH);
+  }
+
+  public int distanceTo() {
+    return Math.abs(x) + Math.abs(y);
+  }
+
+  private void turnLeft(int turns) {
+    for (int i = 0; i < turns; i++) {
+      this.direction = this.leftTurns.get(this.direction);
+    }
+  }
+
+  private void turnRight(int turns) {
+    for (int i = 0; i < turns; i++) {
+      this.direction = this.rightTurns.get(this.direction);
+    }
+  }
+
+  public void processInstruction(String c, int value) {
+    switch (c) {
+      case "F": {
+        if (this.direction == Direction.EAST) {
+          this.x -= value;
+        }
+        if (this.direction == Direction.WEST) {
+          this.x += value;
+        }
+        if (this.direction == Direction.NORTH) {
+          this.y -= value;
+        }
+        if (this.direction == Direction.SOUTH) {
+          this.y += value;
+        }
+        break;
+      }
+      case "E": {
+        this.x -= value;
+        break;
+      }
+      case "W": {
+        this.x += value;
+        break;
+      }
+      case "N": {
+        this.y -= value;
+        break;
+      }
+      case "S": {
+        this.y += value;
+        break;
+      }
+      case "L": {
+        if (value % 90 != 0) {
+          System.out.println(value);
+          throw new IllegalArgumentException("Invalid turn, yo!");
+        }
+        int turns = value / 90;
+        this.turnLeft(turns);
+        break;
+      }
+      case "R": {
+        if (value % 90 != 0) {
+          throw new IllegalArgumentException("Invalid turn, yo!");
+        }
+        int turns = value / 90;
+        this.turnRight(turns);
+        break;
+      }
+      default: {
+        throw new IllegalArgumentException("Unsupported command");
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Ship ship = new Ship(Direction.EAST, 0, 0);
+    Files.lines(Paths.get("testinput.txt")).forEach(l -> {
+      String command = l.substring(0, 1);
+      int value = Integer.parseInt(l.substring(1));
+      ship.processInstruction(command, value);
+    });
+    System.out.println(ship.distanceTo());
+  }
+}
+
+
---
+++ src/main/day12/Waypoint.java
@@ -0,0 +1,102 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Waypoint {
+
+  private int x;
+  private int y;
+  private int shipX;
+  private int shipY;
+
+  public Waypoint(int x, int y, int shipX, int shipY) {
+    this.x = x;
+    this.y = y;
+
+  }
+
+  public int distanceTo() {
+    return Math.abs(shipX) + Math.abs(shipY);
+  }
+
+  private void turnLeft(int turns) {
+    for (int i = 0; i < turns; i++) {
+      int temp = this.x;
+      this.x = this.y * (-1);
+      this.y = temp;
+    }
+  }
+
+  private void turnRight(int turns) {
+    for (int i = 0; i < turns; i++) {
+      int temp = this.y;
+      this.y = this.x * (-1);
+      this.x = temp;
+    }
+  }
+
+  public void printRelativeLocation() {
+    String loc =
+        String.format("shipX: %d, shipY: %d, x: %d, y: %d", this.shipX, this.shipY, this.x, this.y);
+    System.out.println(loc);
+  }
+
+  public void processInstruction(String c, int value) {
+    switch (c) {
+      case "F": {
+        this.shipX += (this.x * value);
+        this.shipY += (this.y * value);
+        break;
+      }
+      case "E": {
+        this.x += value;
+        break;
+      }
+      case "W": {
+        this.x -= value;
+        break;
+      }
+      case "N": {
+        this.y += value;
+        break;
+      }
+      case "S": {
+        this.y -= value;
+        break;
+      }
+      case "L": {
+        if (value % 90 != 0) {
+          System.out.println(value);
+          throw new IllegalArgumentException("Invalid turn, yo!");
+        }
+        int turns = value / 90;
+        this.turnLeft(turns);
+        break;
+      }
+      case "R": {
+        if (value % 90 != 0) {
+          throw new IllegalArgumentException("Invalid turn, yo!");
+        }
+        int turns = value / 90;
+        this.turnRight(turns);
+        break;
+      }
+      default: {
+        throw new IllegalArgumentException("Unsupported command");
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    // 10 units east and 1 unit north
+    Waypoint waypoint = new Waypoint(10, 1, 0, 0);
+    Files.lines(Paths.get("input.txt")).forEach(l -> {
+      String command = l.substring(0, 1);
+      int value = Integer.parseInt(l.substring(1));
+      waypoint.processInstruction(command, value);
+    });
+    waypoint.printRelativeLocation();
+    System.out.println(waypoint.distanceTo());
+  }
+}
+
+
